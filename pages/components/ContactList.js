425f29a7b78d7476fc32bbf9b95904917efafcfd
newalina <157907056+newalina@users.noreply.github.com>
2024-04-28 15:51:17 -0400

    add contact details

---
+++ pages/components/ContactList.js
@@ -0,0 +1,66 @@
+import styles from "./ContactList.module.css";
+
+export default function ContactList({ contacts }) {
+  const mapWarmth = (warmth) => {
+    if (warmth >= 90 && warmth <= 100) {
+      return { warmthTag: "hot", warmthColour: "#f95738" };
+    } else if (warmth >= 70 && warmth < 90) {
+      return { warmthTag: "very warm", warmthColour: "#ee964b" };
+    } else if (warmth >= 50 && warmth < 70) {
+      return { warmthTag: "moderately warm", warmthColour: "#f4D35e" };
+    } else if (warmth >= 30 && warmth < 50) {
+      return { warmthTag: "moderately cool", warmthColour: "#a8dadc" };
+    } else if (warmth >= 10 && warmth < 30) {
+      return { warmthTag: "very cool", warmthColour: "#759fbc" };
+    } else {
+      return { warmthTag: "cold", warmthColour: "#1f5673" };
+    }
+  };
+
+  return (
+    <div className={styles.list}>
+      {contacts.map((contact) => (
+        <div key={contact.id} className="contact">
+          <details className={styles.details}>
+            <summary className={styles.summary}>
+              <span className={styles.contact}>
+                <p>
+                  {contact.firstName} {contact.lastName}
+                </p>
+                <img
+                  className={styles.profile}
+                  src={contact.imageUrl}
+                  height="40"
+                  width="40"
+                  alt={`${contact.firstName} ${contact.lastName}'s profile picture`}
+                />
+              </span>
+            </summary>
+          </details>
+          <div className="content">
+            <div className={styles.info}>
+              <a href={`mailto:${contact.email}`}>{contact.email}</a>
+              {" • "}
+              <a href={`tel:${contact.phone}`}>{contact.phone}</a>
+            </div>
+            <p>
+              You and {contact.firstName} are{" "}
+              <strong style={{ color: mapWarmth(contact.warmth).warmthColour }}>
+                {mapWarmth(contact.warmth).warmthTag}
+              </strong>
+              . You last communicated with them {contact.lastCommunicated}. On
+              average, you interact with them {contact.frequency}, and it’s
+              mostly on {contact.primaryChannel}. Your warmth score with{" "}
+              {contact.firstName} is{" "}
+              <strong style={{ color: mapWarmth(contact.warmth).warmthColour }}>
+                {contact.warmth}
+              </strong>
+              .
+            </p>
+          </div>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,91 +2,10 @@ import { useState, useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import ContactList from "./components/ContactList";
+import contacts from "../public/contacts.json";
 
 export default function Home() {
-  const contacts = [
-    {
-      id: 0,
-      firstName: "Emily",
-      lastName: "Johnson",
-      email: "emily@email.com",
-      phone: "(000) 000-0000",
-      imageUrl: "profile.png",
-    },
-    {
-      id: 1,
-      firstName: "Michael",
-      lastName: "Smith",
-      email: "michael@email.com",
-      phone: "(111) 111-1111",
-      imageUrl: "profile.png",
-    },
-    {
-      id: 2,
-      firstName: "Sarah",
-      lastName: "Williams",
-      email: "sarah@email.com",
-      phone: "(222) 222-2222",
-      imageUrl: "profile.png",
-    },
-    {
-      id: 3,
-      firstName: "Christopher",
-      lastName: "Brown",
-      email: "christopher@email.com",
-      phone: "(333) 333-3333",
-      imageUrl: "profile.png",
-    },
-    {
-      id: 4,
-      firstName: "Jessica",
-      lastName: "Davis",
-      email: "jessica@email.com",
-      phone: "(444) 444-4444",
-      imageUrl: "profile.png",
-    },
-    {
-      id: 5,
-      firstName: "Matthew",
-      lastName: "Wilson",
-      email: "matthew@email.com",
-      phone: "(555) 555-5555",
-      imageUrl: "profile.png",
-    },
-    {
-      id: 6,
-      firstName: "Olivia",
-      lastName: "Taylor",
-      email: "olivia@email.com",
-      phone: "(666) 666-6666",
-      imageUrl: "profile.png",
-    },
-    {
-      id: 7,
-      firstName: "Ethan",
-      lastName: "Martinez",
-      email: "ethan@email.com",
-      phone: "(777) 777-7777",
-      imageUrl: "profile.png",
-    },
-    {
-      id: 8,
-      firstName: "Sophia",
-      lastName: "Anderson",
-      email: "sophia@email.com",
-      phone: "(888) 888-8888",
-      imageUrl: "profile.png",
-    },
-    {
-      id: 9,
-      firstName: "Alexander",
-      lastName: "Garcia",
-      email: "alexander@email.com",
-      phone: "(999) 999-9999",
-      imageUrl: "profile.png",
-    },
-  ];
-
   return (
     <div className={styles.container}>
       <Head>
@@ -98,58 +17,8 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>Contacts</h1>
 
-        <div className={styles.list}>
-          {contacts.map((contact) => (
-            <div key={contact.id} className="contact">
-              <details className={styles.details}>
-                <summary className={styles.summary}>
-                  <span>
-                    <p>
-                      {contact.firstName} {contact.lastName}
-                    </p>
-                    <img
-                      className={styles.profile}
-                      src={contact.imageUrl}
-                      height="40"
-                      width="40"
-                      alt={`${contact.firstName}'s profile picture`}
-                    />
-                  </span>
-                </summary>
-              </details>
-              <div className="content">
-                <div className={styles.info}>
-                  <a href={`mailto:${contact.email}`}>{contact.email}</a>
-                  {" • "}
-                  <a href={`tel:${contact.phone}`}>{contact.phone}</a>
-                </div>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Aliquam dapibus rutrum feugiat. Curabitur dictum ex eget
-                  tellus congue fringilla. In finibus lorem quis lacus vulputate
-                  fermentum. Aliquam tristique felis sed mauris sagittis
-                  pharetra. Integer justo velit, volutpat et semper pulvinar,
-                  scelerisque eget leo.
-                </p>
-              </div>
-              <hr />
-            </div>
-          ))}
-        </div>
+        <ContactList contacts={contacts}></ContactList>
       </main>
-
-      {/* <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer> */}
     </div>
   );
 }
