9b95fa1814d0e00258921d1845218b59e1078a13
newalina <157907056+newalina@users.noreply.github.com>
2024-04-28 23:04:18 -0400

    install particles.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import Script from "next/script";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script
+        src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"
+        strategy="beforeInteractive"
+      />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,14 @@ export default function Home() {
   const [sortedContacts, setSortedContacts] = useState([...contacts]);
 
   useEffect(() => {
+    if (typeof window !== "undefined") {
+      particlesJS.load("particles-js", "/particles.json", function () {
+        console.log("callback - particles.js config loaded");
+      });
+    }
+  }, []);
+
+  useEffect(() => {
     filterAndSort();
   }, [searchTerm, sortBy]);
 
@@ -38,79 +46,75 @@ export default function Home() {
   };
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Contacts</title>
-        <meta name="author" content="newalina" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className={styles.background}>
+      <div id="particles-js" className={styles.particles}></div>
 
-      <main className={styles.main}>
-        <div className={styles.title}>
-          <h1>Contacts</h1>
-          <div className={styles.nav}>
-            <div>
-              <input
-                className={styles.search}
-                type="text"
-                placeholder="Search"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-              />
-            </div>
+      <div id="particles-js" className={styles.container}>
+        <Head>
+          <title>Contacts</title>
+          <meta name="author" content="newalina" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-            <div className={styles.sort}>
-              <fieldset className={styles.fieldset}>
-                <legend>Sort</legend>
-                <div>
-                  <input
-                    type="radio"
-                    id="firstName"
-                    name="sortOptions"
-                    value="firstName"
-                    checked={sortBy === "firstName"}
-                    onClick={() => setSortBy("firstName")}
-                  />
-                  <label for="firstName">First name</label>
-                </div>
+        <main className={styles.main}>
+          <div className={styles.title}>
+            <h1>Contacts</h1>
+            <div className={styles.nav}>
+              <div>
+                <input
+                  className={styles.search}
+                  type="text"
+                  placeholder="Search"
+                  defaultValue={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+              </div>
 
-                <div>
-                  <input
-                    type="radio"
-                    id="lastName"
-                    name="sortOptions"
-                    value="lastName"
-                    checked={sortBy === "lastName"}
-                    onClick={() => setSortBy("lastName")}
-                  />
-                  <label for="lastName">Last name</label>
-                </div>
+              <div className={styles.sort}>
+                <fieldset className={styles.fieldset}>
+                  <legend>Sort</legend>
+                  <div>
+                    <input
+                      type="radio"
+                      id="firstName"
+                      name="sortOptions"
+                      value="firstName"
+                      checked={sortBy === "firstName"}
+                      onChange={() => setSortBy("firstName")}
+                    />
+                    <label htmlFor="firstName">First name</label>
+                  </div>
 
-                <div>
-                  <input
-                    type="radio"
-                    id="warmth"
-                    name="sortOptions"
-                    value="warmth"
-                    checked={sortBy === "warmth"}
-                    onClick={() => setSortBy("warmth")}
-                  />
-                  <label for="warmth">Warmth</label>
-                </div>
-              </fieldset>
+                  <div>
+                    <input
+                      type="radio"
+                      id="lastName"
+                      name="sortOptions"
+                      value="lastName"
+                      checked={sortBy === "lastName"}
+                      onChange={() => setSortBy("lastName")}
+                    />
+                    <label htmlFor="lastName">Last name</label>
+                  </div>
+
+                  <div>
+                    <input
+                      type="radio"
+                      id="warmth"
+                      name="sortOptions"
+                      value="warmth"
+                      checked={sortBy === "warmth"}
+                      onChange={() => setSortBy("warmth")}
+                    />
+                    <label htmlFor="warmth">Warmth</label>
+                  </div>
+                </fieldset>
+              </div>
             </div>
           </div>
-        </div>
-
-        {/* <div className={styles.nav}>
-          <h3>Sort by</h3>
-          <div onClick={sortByFirstName}>First name</div>
-          <div onClick={sortByLastName}>Last name</div>
-          <div onClick={sortByWarmth}>Warmth</div>
-        </div> */}
-
-        <ContactList contacts={sortedContacts}></ContactList>
-      </main>
+          <ContactList contacts={sortedContacts}></ContactList>
+        </main>
+      </div>
     </div>
   );
 }
